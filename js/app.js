3270d5fd521c35f9d63f2741cfbeaca668315449
ciaran kehoe <ciarankehoe22@gmail.com>
2018-09-19 00:18:39 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', 'TodoService', 'TodoDirective']);
+
+angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	})
+	.when('/accounts/register', {
+		templateUrl: 'templates/register.html',
+		controller: 'RegisterController'
+	})
+	.when('/todo', {
+		templateUrl: 'templates/todo.html',
+		controller: 'TodoController'
+	}).when('/todo/edit/:id', {
+	    templateUrl:'templates/edit-todo.html',
+	    controller: 'EditTodoController'
+	});
+});
---
+++ js/service.js
@@ -0,0 +1,32 @@
+angular.module('UserService', [])
+    .factory('UserAPIService', function($http) {
+        UserAPIService = {
+            callAPI: function(url, data) {
+                return $http.post(url, data);
+            }
+        };
+        return UserAPIService;
+    });
+
+angular.module('TodoService', [])
+    .factory('TodoAPIService', function($http) {
+        TodoAPIService = {
+            getTodos: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.get(url, {params:{"username": data}}, header);
+            },
+            createTodo: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.post(url, data, header);
+            },
+            editTodo: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.put(url, data, header);
+            },
+            deleteTodo: function(url, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.delete(url, token, header);
+            }
+        };
+        return TodoAPIService;
+    });
